59b3b2ef8caf51009731cb807bae4635f5620477
Kulo Kenci <kulo.kenci@gmail.com>
2019-11-29 13:25:41 +0700

    nambah videojs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 // Head
 document.write('<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/mdui@0.4.3/dist/css/mdui.min.css">');
 document.write('<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/kulokenci/goindex-drive@2.0/custom.css">');
+document.write('<link rel="stylesheet" href="//vjs.zencdn.net/7.5.5/video-js.css">');
+document.write('<link rel="stylesheet" href="//vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js">');
 // Markdown
 document.write('<script src="//cdn.jsdelivr.net/npm/markdown-it@9.1.0/dist/markdown-it.min.js"></script>');
 document.write('<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>*{padding:0 6px;margin:0 2px}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>.mdui-typo-headline{padding:0 1pc 0 0}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}.mdui-container{width:100%!important;margin:0}.mdui-toolbar>.mdui-typo-headline,.mdui-toolbar>a:last-child,.mdui-toolbar>i:first-child{display:block}}</style>');
@@ -176,8 +178,6 @@ function file(path) {
 		return file_code(path);
 	}
 	if ("|mp4|".indexOf(`|${ext}|`) >= 0) {
-		document.write('<link rel="stylesheet" href="//vjs.zencdn.net/7.5.5/video-js.css">');
-		document.write('<link rel="stylesheet" href="//vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js">');
 		return file_video(path);
 	}
 	if ("|bmp|jpg|jpeg|png|gif|".indexOf(`|${ext}|`) >= 0) {
